18834418692fa03b7e4a4c47e29b9f4535dcd40e
Angelica-92 <Angelica_92-@hotmail.com>
2015-06-04 14:28:40 +0200

    Början till att lägga till, och ta bort

--- Webbsidan/databas.py
+++ Webbsidan/databas.py
@@ -1,19 +1,66 @@
 # coding: utf-8
 import mysql.connector
 
-
 #connect
 db = mysql.connector.connect(host="195.178.235.60",
 user="ae2947", passwd="Nilorak96", db="ae2947")
 cursor=db.cursor()
 
-def Get_employee():
-    cursor.execute("Select EmployeeID, SSN, Name, PhoneNO from Employee order by EmployeeID")
-    #get the resultset as a tuple
+def Get_band():
+    '''Läser från databasen och hämtar allt som finns i tabellen Band'''
+    '''Lägger alla variabler i en lista och returnerar värdet'''
+    
+    cursor.execute("Select Bandname, Genre, Country from Band order by Bandname")
     result = cursor.fetchall()
-    for employee in result:
-        print employee [0], employee [1], employee [2], employee [3]
+    
+    bandlist=[]
+    l = 0
 
+    for bandcolumn in bandlist:
+        bandlist.append(bandcolumn)
+        print bandcolumn [l]
+        l = l+1
+    return bandlist
+    #get the resultset as a tuple
+
+def add_employee():
+
+    #förbereder ett cursor objekt genom att använda cursor() metod
+    cursor =db.cursor()
+
+    '''Insert, lägger till värden till Employee tabellen'''
+    sql =("insert into EmployeeSSN, Name, PhoneNO values ('196812012432','Lois Morran', '07623423212'")
+    
+    try:
+        #Genoför Sql funktionen
+        cursor.execute(sql)
+        #Lägger till ändringen i databasen
+        db.commit()
+    except:
+        #Gör en rollback om det blir fel
+        db.rollback()
+
+        #stänger databasen
+        db.close()
+    
+def remove_employee():
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Prepare SQL query to DELETE required records
+    sql = "DELETE FROM EMPLOYEE WHERE Name > '%d'" % (20)
+    try:
+       # Execute the SQL command
+       cursor.execute(sql)
+       # Commit your changes in the database
+       db.commit()
+    except:
+       # Rollback in case there is any error
+       db.rollback()
+
+       # disconnect from server
+       db.close()
+    
 def Get_band():
     cursor.execute("Select Bandname, Genre, Country, Contact from Band order by Bandname")
     result = cursor.fetchall()
